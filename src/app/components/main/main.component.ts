62a81ebf906ebe519ebaa72befbac7fe5c3419f4
Karthik Nagaraj Naik <kanan@eg.dk>
2024-05-31 01:27:54 +0530

    main content done json pending

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -5,8 +5,26 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './main.component.html',
-  styleUrl: './main.component.scss'
+  styleUrl: './main.component.scss',
 })
 export class MainComponent {
+  cardFunc = async (iid: number) => {
+    const cardWindow = document.getElementById('card-frame');
+    const cardDescription = document.getElementById('card-desc');
+    const user = document.getElementById('card-author');
+    const views = document.getElementById('card-view');
+    const DOU = document.getElementById('card-DOU');
 
+    const dataJson = await fetch(`http://localhost:3000/entries/${iid}`);
+    const data = await dataJson.json();
+    if (cardWindow && cardDescription && user && views && DOU) {
+      cardWindow.setAttribute('src', data.pdfLink);
+      console.log(data);
+      cardDescription.innerText = data.description;
+      user.innerText = data.uploader;
+      views.innerText = data.views;
+      DOU.innerText = data.DOU;
+      return;
+    }
+  };
 }
